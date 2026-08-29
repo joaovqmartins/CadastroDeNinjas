d18fd77a0cb51c7dd2a2c527e930790d6c4b3595
joaovqmartins <joavmartin3zp1n@gmail.com>
2026-08-29 13:13:27 -0300

    feat: implementa NinjaRepository

--- src/main/java/dev/java10x/CadastroDeNinjas/Repository/NinjaRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Repository/NinjaRepository.java
@@ -1,4 +1,7 @@
 package dev.java10x.CadastroDeNinjas.Repository;
 
-public class NinjaRepository {
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
