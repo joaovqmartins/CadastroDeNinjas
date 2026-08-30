cb46344ba98674cfb600df185d56688a0be1003b
joaovqmartins <joavmartin3zp1n@gmail.com>
2026-08-30 14:43:08 -0300

    feat: implementa services de ninjas e missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/Service/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Service/NinjaService.java
@@ -1,4 +1,49 @@
 package dev.java10x.CadastroDeNinjas.Service;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import dev.java10x.CadastroDeNinjas.Repository.NinjaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // Listar
+    public List<NinjaModel> listarNinjas() {
+    return ninjaRepository.findAll();
+    }
+
+    // Listar por ID
+    public NinjaModel listarNinjasPorId(Long id) {
+        Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
+        return ninjaPorId.orElse(null);
+    }
+
+    // Criar um novo ninja
+    public NinjaModel criarNinja(NinjaModel ninja) {
+        return ninjaRepository.save(ninja);
+    }
+
+    // Criar um novo ninja
+    public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado) {
+        if (ninjaRepository.existsById(id)) {
+            ninjaAtualizado.setId(id);
+            return ninjaRepository.save(ninjaAtualizado);
+        }
+        return null;
+    }
+
+    // Deletar um ninja
+    public void deletarNinjaPorId(Long id) {
+    }
+
 }
+
