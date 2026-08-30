b6aac65109b9b6df524262b174a3fa71a7410d15
joaovqmartins <joavmartin3zp1n@gmail.com>
2026-08-30 11:57:19 -0300

    feat: adiciona controller de missoes

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller/MissoesController.java
@@ -0,0 +1,32 @@
+package dev.java10x.CadastroDeNinjas.Controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Missões listadas com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada com sucesso";
+    }
+
+    @DeleteMapping
+    public String deletarMissao() {
+        return "Missão deletada com sucesso";
+    }
+
+
+}
