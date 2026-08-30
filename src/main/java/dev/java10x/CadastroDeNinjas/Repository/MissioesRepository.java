477a341f33c0b367ebfc721c6f81f58f8949f435
joaovqmartins <joavmartin3zp1n@gmail.com>
2026-08-30 16:37:24 -0300

    refactor: atualiza repositories para novo pacote

--- src/main/java/dev/java10x/CadastroDeNinjas/Repository/MissioesRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Repository/MissioesRepository.java
@@ -1,6 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Repository;
 
-import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import dev.java10x.CadastroDeNinjas.Model.MissoesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissioesRepository extends JpaRepository<MissoesModel, Long> {
